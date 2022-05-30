e6ea24fae211ac0c3d390bd3e8867cc9d23cfe49
MSlezak2 <37062938+MSlezak2@users.noreply.github.com>
2022-05-30 12:17:29 +0200

    Update main.c
    Repair previous mistakes

--- TW-1-test/main.c
+++ TW-1-test/main.c
@@ -1,19 +1,77 @@
 #include <stdio.h>
 
-//
+// Stan gry:
+char board[3][3];
+
+int difficulty; // można enum, będzie profesjonalniej
+int game_mode;
+char name_1[50];
+char token; // znacznik
+//...
+int no_moves = 0;
+//...
+
 
 int main()
 {
+	// Ekran powitalny - użytkownik wybiera opcje itd...
 
+	/****tryb gry****/
+
+	/****trudność****/
+
+	/****wybór imion****/
+
+	/****wybór znacznika****/
+
+	/****start gry****/
+
+	// Pętla gry
 	do
 	{
+		/****wyczyść ekran****/
+		/****wyświetl planszę****/
 
-		//wy?wietl plansz?
+		// gracz 1:
+		switch (game_mode)
+		{
+			case 1: // player vs player
+		/****zapytaj o współrzędne****/
+		/****sprawdź czy dobre i wyświetl feedback****/
+			break;
 
+			case 2: // player vs computer
+		/****zapytaj o współrzędne****/
+		/****sprawdź czy dobre i wyświetl feedback****/
+				break;
 
+			case 3: // computer vs computer
+		/****ruch komputera (w zależności od poziomy trudności)****/
+				break;
+		}
 
-	} while ( warunek_stopu );
+		// gracz 2:
+		switch (game_mode)
+		{
+		case 1: // player vs player
+	/****zapytaj o współrzędne****/
+	/****sprawdź czy dobre i wyświetl feedback****/
+			break;
 
+		case 2: // player vs computer
+	/****ruch komputera (w zależności od poziomy trudności)****/
+			break;
+
+		case 3: // computer vs computer
+	/****ruch komputera (w zależności od poziomy trudności)****/
+			break;
+		}
+
+		/****inkrementacja zmiennej no_moves****/
+
+	} while ( !(/****wygrana?****/) || !(/****remis?****/) );
+
+	/****wyświetl wynik i statystyki****/
 
 	return 0;
 }
