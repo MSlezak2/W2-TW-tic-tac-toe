eade1ac4990c9efcb20a09fb993c5a87a323b6cb
MSlezak2 <37062938+MSlezak2@users.noreply.github.com>
2022-05-30 13:17:39 +0200

    Update main.c
    Final version of the template

--- TW-1-test/main.c
+++ TW-1-test/main.c
@@ -1,43 +1,55 @@
 #include <stdio.h>
 
 // Stan gry:
-char board[3][3];
+char board[3][3]; // 'X' / 'O' / '_'
 
 int difficulty; // można enum, będzie profesjonalniej
 int game_mode;
 char name_1[50];
+char name_2[50];
 char token; // znacznik
 //...
-int no_moves = 0;
+int num_moves = 0;
 //...
 
+int choose_game_mode();
 
 int main()
 {
 	// Ekran powitalny - użytkownik wybiera opcje itd...
 
 	/****tryb gry****/
+	game_mode = choose_game_mode();
 
 	/****trudność****/
+	difficulty = choose_difficulty();
 
 	/****wybór imion****/
 
 	/****wybór znacznika****/
 
 	/****start gry****/
+	printf("Press any key to start the game...");
+	getch();
+
+	/****wyczyszczenie planszy****/
 
 	// Pętla gry
 	do
 	{
 		/****wyczyść ekran****/
+		system("cls");
 		/****wyświetl planszę****/
+		display_board();
 
 		// gracz 1:
 		switch (game_mode)
 		{
 			case 1: // player vs player
 		/****zapytaj o współrzędne****/
+
 		/****sprawdź czy dobre i wyświetl feedback****/
+
 			break;
 
 			case 2: // player vs computer
@@ -67,11 +79,22 @@ int main()
 			break;
 		}
 
-		/****inkrementacja zmiennej no_moves****/
+		/****inkrementacja zmiennej num_moves****/
 
 	} while ( !(/****wygrana?****/) || !(/****remis?****/) );
 
-	/****wyświetl wynik i statystyki****/
+	/****wyświetl wynik****/
+
+	/****statystyki****/
+
+	/****zapytaj czy chce kontynuować****/
 
 	return 0;
+}
+
+int choose_game_mode()
+{
+	int mode = 0;
+
+	return mode;
 }
